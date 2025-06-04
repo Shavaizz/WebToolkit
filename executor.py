6daf11e833619c0355e3c28efcc3a481dd0663d1
Shavaiz <58118381+Shavaizz@users.noreply.github.com>
2025-06-04 14:28:31 +0500

    Update executor.py

--- executor.py
+++ executor.py
@@ -3,9 +3,7 @@ from website_technology import main as webtech
 from website_theme_checker import main as webtheme
 from hostio import main as webhost
 from domainage import main as domainage
-from pagespeed import main as speedcheck
 webstatus()
 webhost()
 domainage()
 webtech()
-speedcheck()
